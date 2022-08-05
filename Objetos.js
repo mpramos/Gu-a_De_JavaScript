b182dac4e604e5cd67773989ecc0832c9cb1cf28
PaolaRamosGutierrez <paolagutierrezramos5@gmail.com>
2022-08-05 02:15:15 -0400

    Keys,values y entries en Objetos

--- Objetos.js
+++ Objetos.js
@@ -24,3 +24,13 @@ console.log('persona',persona)
 //! Obtencion de valores de un objeto
 console.log('obteniendo el primer nombre ',persona.PrimerNombre)
 console.log('obteniendo los nombres a partir de la funcion Obtener ',persona.obtenerNombreCompleto())
+//! Obtención de claves de objetos mediante Object.keys()
+const keys=Object.keys(persona)
+console.log(keys)
+//! Object,values para obtener los valores
+const valores=Object.values(persona)
+console.log('valores')
+console.log(valores)
+//! valores y las propiedades
+const entradas= Object.entries(persona)
+console.log(entradas)
